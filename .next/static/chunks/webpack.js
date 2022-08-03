99db9830da8bdb15cbd09c261652f67561a29078
toto <50374594+toine08@users.noreply.github.com>
2022-08-03 21:48:16 +0200

    designed updated for responsive, i won the battle against footer🫡

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ec3b64d96315cb9"; }
+/******/ 		__webpack_require__.h = function() { return "6c8f7dc2095863ef"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -249,7 +249,7 @@ export default function Home() {
           <h1 className="text-3xl font-semibold">Memos received</h1>
         )}
 
-        <div className="grid gap-3 grid-cols-1 hover:auto-col-max">
+        <div className="grid gap-3 grid-cols-4 hover:auto-col-max sm:grid-cols-2">
           {currentAccount &&
             memos.map((memo, idx) => {
               return (
